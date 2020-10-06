8bb54317bd9ff0f44c0dfc3d6319973b1e5d44f7
flrnllpnt <71372305+flrnllpnt@users.noreply.github.com>
2020-10-06 10:02:53 +0700

    Add files via upload

---
+++ for_loop.java
@@ -0,0 +1,50 @@
+package com.pboreg;
+
+public class for_loop {
+
+    public static void main(String[] args) {
+
+//        for (inisialisasi; kondisi; step_nilai){
+//
+//            aksi;
+//
+//        }
+        System.out.println("ini adalah awal program");
+
+        System.out.println("loop pertama");
+
+        for(int nilai = 0; nilai <= 10; nilai++) {
+
+            System.out.println("for loop ke-" + nilai);
+
+        }
+
+        System.out.println("loop kedua");
+
+        for(int nilai = 0; nilai < 10; nilai++) {
+
+            System.out.println("for loop ke-" + nilai);
+
+        }
+
+        System.out.println("loop ketiga");
+
+        for(int nilai = 10; nilai >= 5; nilai--) {
+
+            System.out.println("for loop ke-" + nilai);
+
+        }
+
+        System.out.println("loop keempat");
+
+
+        for(int nilai = 0; nilai < 10; nilai++) {
+
+            System.out.println("for loop ke-" + nilai);
+
+        }
+
+        System.out.println("ini adalah akhir program");
+
+        }
+    }
---
+++ while_loop.java
@@ -0,0 +1,25 @@
+package com.pboreg;
+
+public class while_loop {
+    public static void main(String[] args) {
+
+//        while (kondisi){
+//            aksi
+//        }
+        int a = 0;
+        boolean kondisi = true;
+
+        System.out.println("Awal program");
+
+        while (kondisi) {
+            System.out.println("while loop ke-" + a);
+            a++;
+
+            if (a == 10){
+                kondisi = false;
+            }
+        }
+
+        System.out.println("Akhir program");
+    }
+}
